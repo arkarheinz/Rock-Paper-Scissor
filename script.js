23298ec7d5d328c47ea23cf49268d7174774528f
Arkar Hein <arkarhein14314@gmail.com>
2024-06-16 10:39:48 +0700

    Add button to playRound and userChoice
    add event listener to playRound and make userChoice when it clicked.

--- script.js
+++ script.js
@@ -11,64 +11,95 @@ const getComputerChoice = () => {
     }
 };
 
-// function to play 5 round to win game.
-const playGame = () => {
-    // variations for scores
-    let userScore = 0;
-    let computerScore = 0;
-    let matchPlay = 0;
 
-    // loop to play 5 round to win
-    for (let round = 1 ; round <= 5 ; round++) {
-        // get user's input to play with computer
-        const getUserChoice = prompt(`Choose your weapon to fight with computer! You can chose rock, paper and scissor.`)
+const userChoice = document.querySelector("#userChoice");
+const playRound = document.querySelector("#playRound")
 
-        // function to play round
-        const playRound = (userChoice, computerChoice) => {
-            userChoice = getUserChoice.toLowerCase();
-            computerChoice = getComputerChoice();
+playRound.addEventListener("click", () => {
+    // get user's input to play with computer
+    const userRock = document.createElement("button");
+    const userPaper = document.createElement("button");
+    const userScissor = document.createElement("button");
 
-            // conditional state for play round including score and choices
-            if (userChoice === computerChoice) {
-                console.log(`Draw! Try again.`);
-                matchPlay++;
-            } else if (
-                userChoice === 'rock' && computerChoice ==='scissor' ||
-                userChoice === 'paper' && computerChoice === 'rock' ||
-                userChoice === 'scissor' && computerChoice === 'paper'
-            ) {
-                userScore++;
-                matchPlay++;
-                console.log(`User Win!!!`);
-            } else if (
-                computerChoice === 'rock' && userChoice ==='scissor' ||
-                computerChoice === 'paper' && userChoice === 'rock' ||
-                computerChoice === 'scissor' && userChoice === 'paper'
-            ) {
-                computerScore++;
-                matchPlay++;
-                console.log(`Computer Win!!!`);
-            }
+        // add attributes to buttons
+        userRock.setAttribute("id", "rock");
+        userRock.textContent = "Rock";
 
-            console.log(`Game${matchPlay}: User Choice: ${userChoice}. Computer Choice: ${computerChoice}.Score: U${userScore} / C${computerScore}`);
-        }
-        
-        // call playRound function
-        playRound();
+        userPaper.setAttribute("id", "paper");
+        userPaper.textContent = "Paper";
 
-        // decide winner after score 3 points
-        if (matchPlay <= 5 && userScore === 3) {
-            console.log('You won the game!!!!!');
-            break;
-        } else if (matchPlay <= 5 && computerScore === 3) {
-            console.log('Computer won the game!!!!!');
-            break;
-        } else if ( matchPlay === 5 && userScore < 3) {
-            console.log(`It is a tie game.`)
-        } else if (matchPlay === 5 && computerScore < 3){
-            console.log(`It is a tie game.`)
-        }
-    }
-}
+        userScissor.setAttribute("id", "scissor");
+        userScissor.textContent = "Scissor";
 
-playGame();
+    userChoice.appendChild(userRock);
+    userChoice.appendChild(userPaper);
+    userChoice.appendChild(userScissor);
+});
+    
+// let getUserChoice = '';
+// // addEventListener to let user choose manually
+// userRock.addEventListener("click", () => {
+//     getUserChoice = 'rock';
+// });
+
+// userPaper.addEventListener("click", () => {
+//     getUserChoice = 'paper';
+// });
+
+// userScissor.addEventListener("click", () => {
+//     getUserChoice = 'scissor';
+// });
+
+// // function to play game
+// const playGame = () => {
+//     // variations for scores
+//     let userScore = 0;
+//     let computerScore = 0;
+//     let matchPlay = 0;
+
+//     // function to play round
+//     const playRound = (userChoice, computerChoice) => {
+//         userChoice = getUserChoice;
+//         computerChoice = getComputerChoice();
+
+//         // conditional state for play round including score and choices
+//         if (userChoice === computerChoice) {
+//             console.log(`Draw! Try again.`);
+//             matchPlay++;
+//         } else if (
+//             userChoice === 'rock' && computerChoice ==='scissor' ||
+//             userChoice === 'paper' && computerChoice === 'rock' ||
+//             userChoice === 'scissor' && computerChoice === 'paper'
+//         ) {
+//             userScore++;
+//             matchPlay++;
+//             console.log(`User Win!!!`);
+//         } else if (
+//             computerChoice === 'rock' && userChoice ==='scissor' ||
+//             computerChoice === 'paper' && userChoice === 'rock' ||
+//             computerChoice === 'scissor' && userChoice === 'paper'
+//         ) {
+//             computerScore++;
+//             matchPlay++;
+//             console.log(`Computer Win!!!`);
+//         }
+
+//         console.log(`Game${matchPlay}: User Choice: ${userChoice}. Computer Choice: ${computerChoice}.Score: U${userScore} / C${computerScore}`);
+//     }
+    
+//     // call playRound function
+//     playRound();
+
+//     // decide winner after score 3 points
+//     if (matchPlay <= 5 && userScore === 3) {
+//         console.log('You won the game!!!!!');
+//     } else if (matchPlay <= 5 && computerScore === 3) {
+//         console.log('Computer won the game!!!!!');
+//     } else if ( matchPlay === 5 && userScore < 3) {
+//         console.log(`It is a tie game.`)
+//     } else if (matchPlay === 5 && computerScore < 3){
+//         console.log(`It is a tie game.`)
+//     }
+// }
+
+// playGame();
